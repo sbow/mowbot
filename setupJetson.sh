427dfcd60e6192de00d13395aa88a433be11ad08
Shaun Bowman <shaun_bowman@hotmail.com>
2017-12-02 23:32:15 -0500

    added IMU / QT Creator

--- setupJetson.sh
+++ setupJetson.sh
@@ -17,13 +17,21 @@ git clone https://github.com/sbow/mowbot.git ~/git/mowbot
 
 # install ros - tx1 repository from jetsonhacks
 git clone https://github.com/jetsonhacks/installROSTX1.git ~/git/installROSTX1
+sudo ~/git/installROSTX1/updateRepositories.sh
 sudo ~/git/installROSTX1/installROS.sh
 #sudo .~/git/installROSTX1/setupCatkinWorkspace.sh
-sudo ~/git/installROSTX1/updateRepositories.sh
 sudo rosdep fix-permissions
 rosdep update
 #source /opt/ros/kinetic/setup.bash
 sudo apt-get install ros-kinetic-tutorials
 sudo apt-get install ros-kinetic-rqt
 sudo apt-get install ros-kenetic-rqt-common-plugins
+
+# get rplidar ROS package
+git clone https://github.com/robopeak/rplidar_ros.git ~/git/rplidar_ros
+
+# get libi2c-dev for accelerometer
+sudo apt-get install libi2c-dev
+sudo apt-get install build-essential qt5-default qtcreator
+git clone https://github.com/jetsonhacks/RTIMULib.git ~/git/RTIMULib
 
